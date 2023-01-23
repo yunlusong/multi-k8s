59cd147d9b9a8e7820052ab3cdf9f9ffbfd58141
Andy Song <andy.yl.song@gmail.com>
2023-01-23 12:25:37 -0500

    change yunlusong to andyylsong in docker image build tag

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker buld -t yunlusong/multi-client:lastest -t yunlusong/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
-docker build -t yunlusong/multi-server:lastest -t yunlusong/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
-docker build -t yunlusong/multi-worker:lastest -t yunlusong/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
-docker push yunlusong/multi-client:latest
-docker push yunlusong/multi-client:$GIT_SHA
-docker push yunlusong/multi-server:latest
-docker push yunlusong/multi-server:$GIT_SHA
-docker push yunlusong/multi-worker:latest
-docker push yunlusong/multi-worker:$GIT_SHA
+docker build -t andyylsong/multi-client:lastest -t andyylsong/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t andyylsong/multi-server:lastest -t andyylsong/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t andyylsong/multi-worker:lastest -t andyylsong/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker push andyylsong/multi-client:latest
+docker push andyylsong/multi-client:$GIT_SHA
+docker push andyylsong/multi-server:latest
+docker push andyylsong/multi-server:$GIT_SHA
+docker push andyylsong/multi-worker:latest
+docker push andyylsong/multi-worker:$GIT_SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=yunlusong/multi-server:$GIT_SHA
-kubectl set image deployments/client-deployment client=yunlusong/multi-client:$GIT_SHA
-kubectl set image deployments/worker-deployment worker=yunlusong/worker-worker:$GIT_SHA
+kubectl set image deployments/server-deployment server=andyylsong/multi-server:$GIT_SHA
+kubectl set image deployments/client-deployment client=andyylsong/multi-client:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=andyylsong/worker-worker:$GIT_SHA
